3776094dc9f6a2fc9f7ff050f1033663009ed621
Shashwath Kattepura Jayabheema Rao (shkattep) <shkattep@cisco.com>
2016-06-23 21:19:09 -0700

    firstCommonAncestor

--- src/treeGs/FirstCommonAncestor_3.java
+++ src/treeGs/FirstCommonAncestor_3.java
@@ -1,6 +1,9 @@
 package treeGs;
 import treeGs.FirstCommonAncestor.Node;
+//there is too much thinking involved here. I may not think of this during interview.
 //no yet complete
+//Buggy -- there will be a problem if the nodes provided are not in the tree
+//simplest solution wud be to verify that nodes exist before u start executing -- but this is not the most efficient way
 
 public class FirstCommonAncestor_3 {
 
@@ -8,7 +11,7 @@ public class FirstCommonAncestor_3 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Node root = FirstCommonAncestor.createTree();
-		Node firstCommonAncestor = commonAncestor(root, root.left.right, root.left.left);
+		Node firstCommonAncestor = commonAncestor(root, root.left.left.left, root.left.left);
 		
 		if(firstCommonAncestor!=null) System.out.println(firstCommonAncestor.data);
 		else System.out.println("Ancestor missing..!!");
@@ -22,6 +25,9 @@ public class FirstCommonAncestor_3 {
 		Node y = commonAncestor(root.right, p, q);
 		if(y!=null && y!=p && y!=q) return y;
 		
+		if(x!=null && y!=null) return root;
+		else if(root==p || root==q) return root;
+		else return x==null? y:x;
 	}
 
 }
